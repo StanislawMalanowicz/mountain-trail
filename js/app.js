626186fca555e3c44c22eff6f012620ccb616826
StanislawMalanowicz <beben86@wp.pl>
2018-11-08 15:07:23 +0100

    WiP

--- js/app.js
+++ js/app.js
@@ -1,25 +1,30 @@
 const hello = $('h1')
-hello.on('click', ()=>{
-    alert('jquerry działa ;)')
-} )
+// hello.on('click', ()=>{
+//     alert('jquerry działa ;)')
+// } )
 
 
 const $reg = $('.register')
 const $name = $('input[type="text"]')
 const $email = $('input[type="email"]')
+const $select = $('select');
+const $check = $('input[type="checkbox"]')
 let nm;
 let em;
+let country;
+let isChecked
 
 
 
 $reg.on('click',() => {
     if ($email.val().indexOf('@') === -1){
         console.log('to nie jest email')
-         em = true;
+        
     }else{
         console.log('to jest email')
-        
+        em = true;
     }
+    
     if ($name.val().length === 0){
         console.log('krotkie to imie')
     }else{
@@ -27,7 +32,22 @@ $reg.on('click',() => {
          nm = true;
     }
 
-    nm && em? alert("szerokiej drogi"): alert('cos tutaj nie gra;(')
+    if(!$select.val()){
+        console.log('wybierz jakiś kraj')
+    }else{
+        console.log('powinno być git')
+        country = true;
+    }
+
+    if($check.attr('checked')){
+        console.log('zaznaczone')
+        isChecked = true;
+    }else{
+        console.log('zaakceptuj regulamin ośle')
+    }
+    nm && em && country && isChecked? alert("szerokiej drogi"): alert('cos tutaj nie gra;(')
+
+        
 })
 
 
